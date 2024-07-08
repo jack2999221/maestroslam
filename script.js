18401622ffaedd9c10cd2edf489a946e9db48f18
jack2999221 <jack_96_marzo@hotmail.com>
2024-07-08 16:41:09 -0600

    20C087C

---
+++ script.js
@@ -0,0 +1,9 @@
+function startQuiz() {
+    alert('Iniciar el Quiz!');
+    // Aquí puedes agregar la lógica para iniciar el quiz.
+}
+
+function showCaseStudies() {
+    alert('Mostrar Casos Clínicos');
+    // Aquí puedes agregar la lógica para mostrar los casos clínicos.
+}
